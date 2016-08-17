298c847c768fe0654bd88ef04663134fcc11644a
JP Quicksall <john.paul.quicksall@gmail.com>
2016-08-17 11:09:13 -0700

    added srs data function

--- methods2.py
+++ methods2.py
@@ -50,33 +50,51 @@ def getRepFunction(lastname, firstname, middle):
   return str(allAccountArray) + '\n' + str(allAccount) 
 
 # Function to load what keys to pull for from the Install Data
-def getInstallKeyList(keyfilename):
+def getInstallKeyList(install_keyfile):
   
-  with open(keyfilename) as keylist_file:
-    keylist = json.load(keylist_file)
+  with open(install_keyfile) as install_keyfile:
+    install_keylist = json.load(install_keyfile)
 
-  print keylist
-  keyarray = []
-  for k, v in keylist.items():
+  installKeyArray = []
+  for k, v in install_keylist.items():
     if v == 'true':
-      keyarray.append(k)
- 
-  print keyarray
+      installKeyArray.append(k.rstrip())
+  print installKeyArray
 
   #keyarray = []
   #for line in keyfile:
   #  keyarray.append(line.rstrip())
   #keyfile.close()
 
-  return  keyarray
+  return installKeyArray
+
+#Function to load SRS key list
+def getSRSKeyList(srskeyfile):
+  
+  with open(srskeyfile) as srskeylist_file:
+    srskeylist = json.load(srskeylist_file)
+
+  print srskeylist
+  srskeyarray=[]
+  for k, v in srskeylist.items():
+    if v == 'true':
+      srskeyarray.append(k.rstrip())
+  print srskeyarray
+
+  return srskeyarray
 
 # Function to pull account Install base, create CSV and upload it to ECS
 def getInstallData(gdun):
   
-  #get list of keys to include on CSV
-  keyArrayInstall = getInstallKeyList('keylistJson.json')
+  #get list of Install keys to include on CSV
+  keyArrayInstall = getInstallKeyList('installkeys.json')
  
   print keyArrayInstall
+
+  #get list of SRS  keys to include on CSV
+  keyArraySRS = getSRSKeyList('srskeys.json')
+  print keyArraySRS
+
 
   #Get intall data 
   url = "http://pnwreport.bellevuelab.isus.emc.com/api/installs/"+gdun
@@ -84,7 +102,7 @@ def getInstallData(gdun):
   installJsonData = json.loads(myResponse.content)
     
   
-  # open a file for writing
+  # open a file for writing install data
 
   installfilename =  'InstallData.csv'
   install_data = open(installfilename,'w')
@@ -100,7 +118,7 @@ def getInstallData(gdun):
     csvwriter.writerow(tempArray)
   install_data.close()
   
-  
+  #push install CSV to ECS
   bucket = 'QBR_Files'
   filename = gdun + '_Install_Data'
   s3 = boto3.resource('s3',
@@ -114,10 +132,37 @@ def getInstallData(gdun):
 
   print 'file ' + gdun + '_Install_Data   was added to the bucket: ' + bucket  
   
+  #Get SRS data
   
- 
+  srsUrl = "http://pnwreport.bellevuelab.isus.emc.com/api/srs/"+gdun
+  srsResponse = requests.get(srsUrl)
+  srsJsonData = json.loads(srsResponse.content)
+  
+  # open a file for writing srs data
+  
+  srsfile = 'SRSData.csv'
+  srsdata = open(srsfile, 'w')
 
-  return "You got the install information for Gdun: {0}".format(gdun)
+
+  #create the csv writer object
+ 
+  SRScsvwriter = csv.writer(srsdata)
+  SRScsvwriter.writerow(keyArraySRS)
+  for item in srsJsonData['rows']:
+    SRStempArray = []
+    for srsSubKey in keyArraySRS:
+      SRStempArray.append(item[srsSubKey])
+    SRScsvwriter.writerow(SRStempArray)
+  srsdata.close()
+
+
+  #push srs CSV to ECS
+  
+  srsfilename = gdun + '_SRS_Data'
+
+  s3.Bucket(bucket).upload_file(srsfile, srsfilename)
+
+  return "You got the install and SRS information for Gdun: {0}".format(gdun)
  
 
 
